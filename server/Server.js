b45b822af7d416f60fa6500ae67ad74f2020320c
AvigayilResnick <avigayilt124@gmail.com>
2025-06-18 20:53:43 +0300

    resolved merge conflict in Server.js and others

--- server/Server.js
+++ server/Server.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import cors from 'cors';
+import pool from './db/dbConnection.js'; 
 import authRoutes from './routes/authRoute.js';
 import clientRoutes from './routes/clientRoute.js';
 import userRoutes from './routes/userRoute.js';
-
-import pool from './db/dbConnection.js'; // Adjust the path as necessary
 import roleRequestRoutes from './routes/roleRequestRoute.js';
 import supplierRoutes from './routes/supplierRoute.js';
 
@@ -29,11 +28,11 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
   });
 
-  app.use('/api/auth', authRoutes);
-  app.use('/api/client', clientRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/client', clientRoutes);
 app.use('/api/users', userRoutes);
-  app.use('/api/roles', roleRequestRoutes);
-  app.use('/api/suppliers', supplierRoutes);
+app.use('/api/roles', roleRequestRoutes);
+app.use('/api/suppliers', supplierRoutes);
 
 
 // Start the server on a specified port or default to 5000
