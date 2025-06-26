8e7c951c0957b460f9ff1a03c41d8ed4b724ff5e
AvigayilResnick <avigayilt124@gmail.com>
2025-06-27 00:07:45 +0300

    'merge of all updates'

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import {getMyInfo, getMyProfile, updateMyProfile, changePasswordController } from '../controllers/userController.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
-import { authorizeUserOrAdmin } from '../middleware/roleMiddleware.js';
+//import { authorizeUserOrAdmin } from '../middleware/roleMiddleware.js';
 
 const router = express.Router();
 
-router.get('/me', authMiddleware, getMyProfile);
-router.put('/me', authMiddleware, updateMyProfile);
+router.get('/myInfo', authMiddleware, getMyProfile);
+router.put('/myInfo', authMiddleware, updateMyProfile);
 router.post('/change-password', authMiddleware, changePasswordController);
-router.get('/myInfo', authMiddleware, getMyInfo);
+router.get('/myInfoForRefresh', authMiddleware, getMyInfo);
 
 
 
